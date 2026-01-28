e5d775326da99cc24bd7b214d4ffd6bdff5fe41b
manojkumar-077 <manoj.k@webknot.in>
2026-01-29 01:07:40 +0530

    Implementing Exception methods

---
+++ demo/src/main/java/com/Assignment/demo/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.Assignment.demo.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
